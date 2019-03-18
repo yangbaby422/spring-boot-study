01539a1337d00ce516ff52550dca600acc33397c
yangchen <543748493@qq.com>
2019-03-18 09:37:31 +0800

    quickstart add module

---
+++ spring-boot-study/quickstart/src/main/java/com/springboot/quickstart/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.springboot.quickstart.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * SpringBoot的第一个RESTTful请求
+ */
+@RestController
+public class HelloController {
+    @RequestMapping(value = "/hello",method = RequestMethod.GET)
+    public String getHello(){
+        return "Hello,Spring Boot~~";
+    }
+}
