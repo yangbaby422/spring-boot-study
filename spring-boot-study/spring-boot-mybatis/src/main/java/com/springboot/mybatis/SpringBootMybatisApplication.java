5360589dc0d4adf252a18a20664b1f70926017e1
yangchen <543748493@qq.com>
2019-03-18 10:55:57 +0800

    spring-boot-mybatis add module

---
+++ spring-boot-study/spring-boot-mybatis/src/main/java/com/springboot/mybatis/SpringBootMybatisApplication.java
@@ -0,0 +1,16 @@
+package com.springboot.mybatis;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.springboot.mybatis.mapper")
+public class SpringBootMybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMybatisApplication.class, args);
+    }
+
+}
